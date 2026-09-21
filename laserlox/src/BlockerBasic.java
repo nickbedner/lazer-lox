b4fb6bb6267ad31c9384b906081c9a428b68d1ff
Marcus Moore <macum98@gmail.com>
2026-09-21 15:40:55 -0700

    Commenting
    Added comments to BlockerBasic and LaserBasic

--- laserlox/src/BlockerBasic.java
+++ laserlox/src/BlockerBasic.java
@@ -4,13 +4,23 @@ public class BlockerBasic implements LoxNode {
     private int[] posAbsolute;
     private int type;
 
-    public BlockerBasic() {
-        this.ring = 1;
-        this.pos = 0;
-        this.posAbsolute = new int[]{0, 0};
+    //Quick and dirty simple blocker.
+    public BlockerBasic(int ring, int pos){
+        if (ring < 1 || ring > 3) {
+            throw new IllegalArgumentException("There are 3 Player Rings, so the ring must be between 1 and 3.");
+        } else{
+            this.ring = ring;
+        }
+        if (pos < 0 || pos > 11) {
+            throw new IllegalArgumentException("There are 12 slots in a ring, so the position must be between 0 and 11.");
+        } else {
+            this.pos = pos;
+        }
+        this.posAbsolute = new int[]{0,0};
         this.type = 4;
     }
 
+    //For the weird Blocker Variants if we make any.
     public BlockerBasic(int ring, int pos, int[] posAbsolute) {
         if (ring < 1 || ring > 3) {
             throw new IllegalArgumentException("There are 3 Player Rings, so the ring must be between 1 and 3.");
@@ -30,21 +40,26 @@ public class BlockerBasic implements LoxNode {
         }
     }
 
+    //Get the Ring the Blocker is in.
     @Override
     public int GetRing() {
         return ring;
     }
 
+    //Get the Ring Position the Blocker is in.
     @Override
     public int GetPos() {
         return pos;
     }
 
+    //Get the screen position of this Blocker.
     @Override
     public int[] GetPosAbsolute() {
         return posAbsolute;
     }
 
+    //Get what type of Blocker this is.
+    //4 - Basic Blocker
     @Override
     public int GetType() {
         return type;
--- laserlox/src/LaserBasic.java
+++ laserlox/src/LaserBasic.java
@@ -3,16 +3,24 @@ public class LaserBasic implements LoxNode {
     private int pos;
     private int[] posAbsolute;
     private int type;
-    private boolean blocked;
 
-    public LaserBasic() {
-        this.ring = 1;
-        this.pos = 0;
-        this.posAbsolute = new int[]{0, 0};
+    //Quick and dirty simple laser.
+    public LaserBasic(int ring, int pos){
+        if (ring < 1 || ring > 3) {
+            throw new IllegalArgumentException("There are 3 Player Rings, so the ring must be between 1 and 3.");
+        } else{
+            this.ring = ring;
+        }
+        if (pos < 0 || pos > 11) {
+            throw new IllegalArgumentException("There are 12 slots in a ring, so the position must be between 0 and 11.");
+        } else {
+            this.pos = pos;
+        }
+        this.posAbsolute = new int[]{0,0};
         this.type = 1;
-        blocked = false;
     }
 
+    //For the weird Laser Variants if we make any.
     public LaserBasic(int ring, int pos, int[] posAbsolute, int type) {
         if (ring < 1 || ring > 3) {
             throw new IllegalArgumentException("There are 3 Player Rings, so the ring must be between 1 and 3.");
@@ -30,34 +38,30 @@ public class LaserBasic implements LoxNode {
         } else {
             this.type = type;
         }
-        blocked = false;
     }
 
-    public void setBlocked(boolean blocked) {
-        this.blocked = blocked;
-    }
-
+    //Get the Ring the Laser is in.
     @Override
     public int GetRing() {
         return ring;
     }
 
+    //Get the Ring Position the Laser is in.
     @Override
     public int GetPos() {
         return pos;
     }
 
+    //Get the screen position of this Laser.
     @Override
     public int[] GetPosAbsolute() {
         return posAbsolute;
     }
 
+    //Get what type of Laser this is.
+    //1 - Basic Laser
     @Override
     public int GetType() {
         return type;
-    }
-
-    public boolean isBlocked() {
-        return blocked;
     }
 }
